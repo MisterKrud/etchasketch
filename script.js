203280cfa3cf359eacedbf1cc0c53f834fbb9108
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-28 08:59:28 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let gridLH = 16;
+
+let container = document.querySelector("#container"); 
+let div
+//let divArray = []
+function createDiv(number) {
+for (let i=0; i<number; i++) {
+  div +=  d.createElement("div");
+  div.textContent = "text"
+  
+  container.appendChild(div)
+ 
+} 
+}
+createDiv(16) ;
+
