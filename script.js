90e49180445595e0706cd855f1ac62525ecb16cf
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-28 22:38:32 +0000

    Add user defined number of divs in js
    The code was right, my emulator was wrong.
    Also added some temporary styling and text
    
    Plans for next time: add a class to div
    Put stlying in CSS file

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ let div
 //let divArray = []
 function createDiv(number) {
 for (let i=0; i<number; i++) {
-  div +=  d.createElement("div");
+  let div = document.createElement("div")
   div.textContent = "text"
+  div.setAttribute(
+    "style",
+    "background-color: #ffaadd; border: white, solid, 1px; flex-grow: 1;flex-wrap: wrap"
+
+  )
   
   container.appendChild(div)
  
