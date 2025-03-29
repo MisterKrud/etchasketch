7f9275bc77481de3eef0457c2fbe9028db09bd84
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-29 11:51:38 +0000

    Force grid to square divs
    Coerce divs to sa square with css aspect.ratio

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let container = document.querySelector("#container");
 function createDivRow(number) {
 for (let i=0; i<number; i++) {
   let divRow = document.createElement("div")
-  divRow.textContent = "text"
+ // divRow.textContent = "text"
   divRow.setAttribute(
     "style",
     "background-color: #ffaadd; border: white, solid, 1px; flex-grow: 1;"
@@ -18,7 +18,7 @@ for (let i=0; i<number; i++) {
 for (let i=0; i<number; i++) {
   let divCell = document.createElement("div")
   divCell.setAttribute("style", "background-color: #ddFFFF; border: lightGray, solid, white;")
-  divCell.textContent="cell"
+  //divCell.textContent="cell"
 
   divCell.setAttribute("class", "divCell")
   divRow.appendChild(divCell)}
