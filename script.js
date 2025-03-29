5b32d157e1b0b196a2454d5e7e3f077e0108a95a
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-29 22:58:24 +0000

    create grid container and styling
    Put grid in it's own container
    Styling is now easier.
    Fixed border so outside is now 2px
    (combined with the 1px border in the rows/cells)
    
    Also centered some stuff and added flexbox styles

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 let divGrid = 16;
 
 let container = document.querySelector("#container"); 
+const gridContainer = document.createElement("div");
+gridContainer.setAttribute("id","grid-container")
+  container.appendChild(gridContainer);
 
-function createDivGrid(number=gridLH) {
+function createDivGrid(number=divGrid) {
+  
 for (let i=0; i<number; i++) {
   let divRow = document.createElement("div")
  // divRow.textContent = "text"
-  divRow.setAttribute(
-    "style",
-    "background-color: #ffaadd; border: white, solid, 1px; flex-grow: 1;"
-  )
+  //divRow.setAttribute(
+    //"style",
+    //"background-color: #ffaadd; border: white, solid, 1px; flex-grow: 1;"
+  //)
   divRow.setAttribute("class", "divRow");
   
-  container.appendChild(divRow)
+  gridContainer.appendChild(divRow)
 
 
 for (let i=0; i<number; i++) {
   let divCell = document.createElement("div")
-  divCell.setAttribute("style", "background-color: #ddFFFF; border: lightGray, solid, white;")
-  //divCell.textContent="cell"
+ // divCell.setAttribute("style", "border: Gray, solid, 2px;")
+ // divCell.textContent="cell"
 
   divCell.setAttribute("class", "divCell")
   divRow.appendChild(divCell)}
@@ -29,5 +33,6 @@ for (let i=0; i<number; i++) {
 createDivGrid();
 
 const controls = document.createElement("div");
+controls.setAttribute("id", "controls")
 controls.textContent = "Placeholder text";
 container.appendChild(controls)
