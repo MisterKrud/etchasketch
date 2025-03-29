410d870e256977bf0da124b1890ecce356f57962
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-03-29 11:23:09 +0000

    Add 16x16 cell function

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 let gridLH = 16;
 
 let container = document.querySelector("#container"); 
-let div
-//let divArray = []
-function createDiv(number) {
-for (let i=0; i<number; i++) {
-  let div = document.createElement("div")
-  div.textContent = "text"
-  div.setAttribute(
-    "style",
-    "background-color: #ffaadd; border: white, solid, 1px; flex-grow: 1;flex-wrap: wrap"
 
+function createDivRow(number) {
+for (let i=0; i<number; i++) {
+  let divRow = document.createElement("div")
+  divRow.textContent = "text"
+  divRow.setAttribute(
+    "style",
+    "background-color: #ffaadd; border: white, solid, 1px; flex-grow: 1;"
   )
+  divRow.setAttribute("class", "divRow");
   
-  container.appendChild(div)
- 
+  container.appendChild(divRow)
+
+
+for (let i=0; i<number; i++) {
+  let divCell = document.createElement("div")
+  divCell.setAttribute("style", "background-color: #ddFFFF; border: lightGray, solid, white;")
+  divCell.textContent="cell"
+
+  divCell.setAttribute("class", "divCell")
+  divRow.appendChild(divCell)}
 } 
 }
-createDiv(16) ;
 
+
+createDivRow(16);
